45cbb32716e8540a9ccd67230398315d2c836303
TheMach1neDK <59374945+TheMachineDK@users.noreply.github.com>
2022-12-19 17:30:33 +0100

    Add files via upload

---
+++ ReducedNPC/client.lua
@@ -0,0 +1,28 @@
+-- Declare variables
+local pedModels = {} -- table to store the ped models
+local maxPeds = 100 -- maximum number of peds to allow
+
+-- Create a function to reduce the number of NPC ped models
+function reduceNPCPeds()
+  -- Get a list of all the ped models currently spawned on the server
+  pedModels = ESX.Game.GetPeds()
+
+  -- If the number of ped models is greater than the maximum allowed
+  if #pedModels > maxPeds then
+    -- Loop through the list of ped models and despawn any excess models
+    for i = 1, #pedModels do
+      if i > maxPeds then
+        -- Despawn the ped model
+        ESX.Game.DeletePed(pedModels[i])
+      end
+    end
+  end
+end
+
+-- Run the reduceNPCPeds function every minute
+Citizen.CreateThread(function()
+  while true do
+    Citizen.Wait(60000) -- wait for one minute
+    reduceNPCPeds()
+  end
+end)
